b0b90711c79444bffd305ca8a496678525a1f244
Atif Ayaz Md <atif.md@olacabs.com>
2022-07-27 17:35:32 +0530

    Implemented place order

--- src/main/java/com/group11/fooddelivery/service/CustomerService.java
+++ src/main/java/com/group11/fooddelivery/service/CustomerService.java
@@ -1,28 +1,61 @@
 package com.group11.fooddelivery.service;
 
+import com.group11.fooddelivery.clients.CustomerClient;
+import com.group11.fooddelivery.configure.Constants;
+import com.group11.fooddelivery.model.Order;
+import com.group11.fooddelivery.model.OrdersByUser;
+import com.group11.fooddelivery.model.Restaurant;
 import com.group11.fooddelivery.model.request.PlaceOrderRequest;
 import com.group11.fooddelivery.model.response.PlaceOrderResponse;
-import com.group11.fooddelivery.repository.OrderRepository;
 import com.group11.fooddelivery.repository.OrdersByUsersRepository;
 import com.group11.fooddelivery.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CustomerService {
-    @Autowired
-    OrderRepository orderRepository;
     @Autowired
     OrdersByUsersRepository ordersByUsersRepository;
     @Autowired
     RestaurantRepository restaurantRepository;
+    @Autowired
+    CustomerClient customerClient;
 
-    public PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest)   {
+    public PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest) {
         /*
         1. send an order Id
         2. store data in ordersByUsers and orderDetails
          */
         PlaceOrderResponse placeOrderResponse = new PlaceOrderResponse();
+        Restaurant restaurant = restaurantRepository.findById(placeOrderRequest.getRestaurantId()).orElse(null);
+        assert restaurant != null;
+
+        //Generate random orderId
+        String orderId = UUID.randomUUID().toString();
+
+        //Populate ordersByUser table
+        OrdersByUser ordersByUser = new OrdersByUser();
+        ordersByUser.setOrderId(orderId);
+        ordersByUser.setEmail(placeOrderRequest.getEmail());
+        ordersByUser.setStatus(Constants.orderPlaced);
+        ordersByUsersRepository.save(ordersByUser);
+
+        //Populate orders_info table
+        Order order = new Order();
+        order.setOrderId(orderId);
+        order.setRestaurantId(placeOrderRequest.getRestaurantId());
+        customerClient.populateOrderInfo(order, placeOrderRequest.getSelectedItems());
+
+        placeOrderResponse.setRestaurantName(restaurant.getName());
+        placeOrderResponse.setOrderId(orderId);
+        placeOrderResponse.setSelectedItems(customerClient.buildSelectedItems(placeOrderRequest.getSelectedItems()));
+        int totalPrice = customerClient.calculateTotalPrice(placeOrderRequest.getSelectedItems());
+
+        placeOrderResponse.setTotalPrice(totalPrice);
+        placeOrderResponse.setSuccess(true);
+        placeOrderResponse.setMessage("Order placed. Please find the summary!");
         return placeOrderResponse;
     }
 }
