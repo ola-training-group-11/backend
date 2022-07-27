17dcee898e291ee8502bdf303e026e9eb19f1322
Atif Ayaz Md <atif.md@olacabs.com>
2022-07-27 12:09:19 +0530

    Added @RequestBody & /getProfile

--- src/main/java/com/group11/fooddelivery/controller/CommonController.java
+++ src/main/java/com/group11/fooddelivery/controller/CommonController.java
@@ -1,7 +1,9 @@
 package com.group11.fooddelivery.controller;
 
-import com.group11.fooddelivery.model.request.LoginRequest;
 import com.group11.fooddelivery.model.User;
+import com.group11.fooddelivery.model.request.GetProfileRequest;
+import com.group11.fooddelivery.model.request.LoginRequest;
+import com.group11.fooddelivery.model.response.GetProfileResponse;
 import com.group11.fooddelivery.model.response.LoginResponse;
 import com.group11.fooddelivery.model.response.SignUpResponse;
 import com.group11.fooddelivery.service.CommonService;
@@ -10,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class CommonController {
@@ -17,14 +20,21 @@ public class CommonController {
     CommonService commonService;
 
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<LoginResponse> login(LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
+        System.out.println(loginRequest.getUserName());
         LoginResponse loginResponse = commonService.authenticate(loginRequest);
         return new ResponseEntity<>(loginResponse, HttpStatus.OK);
     }
 
     @PostMapping(value = "/signup", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<SignUpResponse> signUp(User user) {
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody User user) {
         SignUpResponse signUpResponse = commonService.register(user);
         return new ResponseEntity<>(signUpResponse, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/getProfile", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<GetProfileResponse> getProfile(@RequestBody GetProfileRequest getProfileRequest) {
+        GetProfileResponse getProfileResponse = commonService.getProfile(getProfileRequest);
+        return new ResponseEntity<>(getProfileResponse, HttpStatus.OK);
     }
 }
