adacbc0d29d83100e7266976a3bef8bf2356c954
Atif Ayaz Md <atif.md@olacabs.com>
2022-07-28 10:59:21 +0530

    Created client for token authentication

---
+++ src/main/java/com/group11/fooddelivery/clients/AuthenticationClient.java
@@ -0,0 +1,26 @@
+package com.group11.fooddelivery.clients;
+
+import com.group11.fooddelivery.model.User;
+import com.group11.fooddelivery.model.request.Request;
+import com.group11.fooddelivery.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/*
+    Except Signup and login, every other action will need token verification.
+ */
+@Component
+public class AuthenticationClient {
+    @Autowired
+    UserRepository userRepository;
+
+    public boolean verifyToken(Request request, String email) {
+        User user = userRepository.findByEmail(email);
+        if(user==null)  return false;
+        String savedToken = user.getToken();
+        if(savedToken==null)    return false;
+        return Objects.equals(savedToken, request.getToken());
+    }
+}
