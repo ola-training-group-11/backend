b77c20bfcb4c90fd4ee2a310cfd947250ee338ed
shivamkumar78 <shivamprajapati421@gmail.com>
2022-07-28 10:29:55 +0530

    getRestaurants api

--- src/main/java/com/group11/fooddelivery/controller/CustomerController.java
+++ src/main/java/com/group11/fooddelivery/controller/CustomerController.java
@@ -1,11 +1,14 @@
 package com.group11.fooddelivery.controller;
 
+import com.group11.fooddelivery.model.request.LatLongRequest;
 import com.group11.fooddelivery.model.request.PlaceOrderRequest;
+import com.group11.fooddelivery.model.response.LatLongResponse;
 import com.group11.fooddelivery.model.response.PlaceOrderResponse;
 import com.group11.fooddelivery.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +24,14 @@ public class CustomerController {
         PlaceOrderResponse placeOrderResponse = customerService.placeOrder(placeOrderRequest);
         return new ResponseEntity<>(placeOrderResponse, HttpStatus.OK);
     }
+
+    @GetMapping(value = "/getRestaurants",consumes = "application/json",produces = "application/json")
+    public ResponseEntity<LatLongResponse> getRestaurants(@RequestBody LatLongRequest latLongRequest){
+        LatLongResponse latLongResponse=customerService.getRestaurant(latLongRequest);
+        return new ResponseEntity<>(latLongResponse,HttpStatus.OK);
+
+    }
+
+
+
 }
--- src/main/java/com/group11/fooddelivery/service/CustomerService.java
+++ src/main/java/com/group11/fooddelivery/service/CustomerService.java
@@ -1,12 +1,18 @@
 package com.group11.fooddelivery.service;
 
+import com.group11.fooddelivery.model.Restaurant;
+import com.group11.fooddelivery.model.request.LatLongRequest;
 import com.group11.fooddelivery.model.request.PlaceOrderRequest;
+import com.group11.fooddelivery.model.response.LatLongResponse;
 import com.group11.fooddelivery.model.response.PlaceOrderResponse;
 import com.group11.fooddelivery.repository.OrderRepository;
 import com.group11.fooddelivery.repository.OrdersByUsersRepository;
 import com.group11.fooddelivery.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomerService {
@@ -17,12 +23,53 @@ public class CustomerService {
     @Autowired
     RestaurantRepository restaurantRepository;
 
-    public PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest)   {
+    public PlaceOrderResponse placeOrder(PlaceOrderRequest placeOrderRequest) {
         /*
         1. send an order Id
         2. store data in ordersByUsers and orderDetails
          */
         PlaceOrderResponse placeOrderResponse = new PlaceOrderResponse();
         return placeOrderResponse;
+    }
+
+    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+        else {
+            double theta = lon1 - lon2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            if (unit.equals("K")) {
+                dist = dist * 1.609344;
+            } else if (unit.equals("N")) {
+                dist = dist * 0.8684;
+            }
+            return (dist);
+        }
+    }
+
+    public LatLongResponse getRestaurant(LatLongRequest latLongRequest) {
+       LatLongResponse latLongResponse=new LatLongResponse();
+       List<String> restaurantByName=new ArrayList<>();
+        List<Restaurant> restaurantList = restaurantRepository.findAll();
+        double lat2=latLongRequest.getLatitude();
+        double lon2=latLongRequest.getLongitude();
+
+        for (int i = 0; i < restaurantList.size(); i++) {
+            double lat1 = restaurantList.get(i).getLatitude();
+            double lon1 = restaurantList.get(i).getLongitude();
+
+            if(distance(lat1,lon1,lat2,lon2,"K")<=5){
+              String res=restaurantList.get(i).getName();
+              restaurantByName.add(res);
+
+            }
+        }
+        latLongResponse.setListOfRestaurant(restaurantByName);
+       return latLongResponse;
+
     }
 }
