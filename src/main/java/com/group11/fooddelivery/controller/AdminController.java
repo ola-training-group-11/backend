00327fee31e520abffda84fdb7d78ea64379a93e
Atif Ayaz Md <atif.md@olacabs.com>
2022-07-29 12:06:13 +0530

    added @CrossOrigin

--- src/main/java/com/group11/fooddelivery/controller/AdminController.java
+++ src/main/java/com/group11/fooddelivery/controller/AdminController.java
@@ -6,12 +6,10 @@ import com.group11.fooddelivery.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@CrossOrigin
 public class AdminController {
     @Autowired
     AdminService adminService;
--- src/main/java/com/group11/fooddelivery/controller/CustomerController.java
+++ src/main/java/com/group11/fooddelivery/controller/CustomerController.java
@@ -12,12 +12,10 @@ import com.group11.fooddelivery.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class CustomerController {
     @Autowired
     CustomerService customerService;
